96ee8c310711ef6566e76359a18a20eff04f7a24
kimberly burton <kj.burton03@gmail.com>
2026-02-08 10:52:49 -0500

    checkin back

--- rockapi/views/rock_view.py
+++ rockapi/views/rock_view.py
@@ -2,7 +2,7 @@ from django.http import HttpResponseServerError
 from rest_framework import serializers, status
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
-from rockapi.models import Rock
+from rockapi.models import Rock, Type
 
 
 class RockView(ViewSet):
@@ -15,9 +15,23 @@ class RockView(ViewSet):
         Returns:
             Response -- JSON serialized instance
         """
-
+        
+        # get an object instance of a rock type
+        chosen_type = Type.objects.get(pk=request.data['typeId'])
+        
+        # create a rock object and assign if property values
+        rock = Rock()
+        rock.user = request.user
+        rock.weight = request.data['weight']
+        rock.name = request.data['name']
+        rock.type = chosen_type
+        rock.save()
+        
+        serialized = RockSerializer(rock, many=False)
+        
+        return Response(serialized.data, status=status.HTTP_201_CREATED)
         # You will implement this feature in a future chapter
-        return Response("", status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        # return Response("", status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
     def list(self, request):
         """Handle GET requests for all items
@@ -31,11 +45,36 @@ class RockView(ViewSet):
             return Response(serializer.data, status=status.HTTP_200_OK)
         except Exception as ex:
             return HttpResponseServerError(ex)
+        
+    def destroy(self, request, pk=None):
+        """Handle DELETE requests for a single item
+
+        Returns:
+            Response -- 200, 404, or 500 status code
+        """
+        try:
+            rock = Rock.objects.get(pk=pk)
+            rock.delete()
+            return Response(None, status=status.HTTP_204_NO_CONTENT)
+
+        except Rock.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+class RockTypeSerializer(serializers.ModelSerializer):
+    """JSON serializer"""
+    
+    class Meta: 
+        model = Type
+        fields = ( 'label', )
 
 
 class RockSerializer(serializers.ModelSerializer):
     """JSON serializer"""
+    type = RockTypeSerializer(many=False)
 
     class Meta:
         model = Rock
-        fields = ( 'id', 'name', 'weight', )
+        fields = ( 'id', 'name', 'weight', 'type', 'user')
